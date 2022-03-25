919ec98772c526ddc8fb2fe2f9433f15e54e457d
Mateo Fay <mateofay2099@gmail.com>
2022-03-25 17:55:54 -0300

    Add theme handling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Home from '@components/Home/Home';
+import { ThemeProvider } from '@providers/Theming.provider.jsx';
 import './App.css';
 
 const App = () => (
-  <main>
-    <Home />
-  </main>
+  <ThemeProvider>
+    <main>
+      <Home />
+    </main>
+  </ThemeProvider>
 );
 
 export default App;
