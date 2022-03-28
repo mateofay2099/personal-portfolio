0299cd4d4994e264503361f701d5f76a47f90f28
Mateo Fay <mateofay2099@gmail.com>
2022-03-28 16:57:28 -0300

    Add font handling

--- src/components/Wrappers/ThemeHandler.jsx
+++ src/components/Wrappers/ThemeHandler.jsx
@@ -1,14 +1,25 @@
 import React, { useEffect } from 'react';
+import WebFont from 'webfontloader';
 import { useThemeContext } from '@providers/Theming.provider.jsx';
 
 const ThemeHandler = ({ children }) => {
   const { currentTheme } = useThemeContext();
+  const PRIMARY_FONT = 'Varela Round';
 
   useEffect(() => {
     Object.entries(currentTheme).forEach(([key, value]) => {
       document.documentElement.style.setProperty(`--${key}`, value);
     });
   }, [currentTheme]);
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: [PRIMARY_FONT],
+      },
+    });
+    document.documentElement.style.setProperty('--primary-font', PRIMARY_FONT);
+  }, []);
 
   return <>{children}</>;
 };
