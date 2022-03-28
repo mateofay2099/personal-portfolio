c800d5803b1dffda8f2ec3eeadc8961f8290afa8
Mateo Fay <mateofay2099@gmail.com>
2022-03-27 22:57:32 -0300

    remove var

--- src/utils/getAge.js
+++ src/utils/getAge.js
@@ -1,8 +1,8 @@
 export const getAge = (birthDateString) => {
-  var today = new Date();
-  var birthDate = new Date(birthDateString);
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
+  const today = new Date();
+  const birthDate = new Date(birthDateString);
+  let age = today.getFullYear() - birthDate.getFullYear();
+  const m = today.getMonth() - birthDate.getMonth();
   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
     age--;
   }
