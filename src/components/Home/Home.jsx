0f97b7dccf854e468a114c4e43abf9c36397d2ea
Mateo Fay <mateofay2099@gmail.com>
2022-03-27 22:51:01 -0300

    Some adjustments

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -22,8 +22,10 @@ const Home = () => {
       <div className="homeIntroductionDiv">
         <p>
           I am {age} years old. I started working on software development when I was 19 years old, although I had
-          already learned the basics of programming in a self-taught way. I love what I do and I am constantly looking
-          for learning new technologies and improving my current skills.
+          already learned the basics of programming in a self-taught way.
+        </p>
+        <p>
+          I love what I do and I am constantly looking for learning new technologies and improving my current skills.
         </p>
       </div>
       <button className="aboutMeButton">More About Me</button>
