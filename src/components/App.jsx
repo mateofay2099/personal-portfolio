d93d0efb16f33be764c4acb7ba0a92cb439603da
Mateo Fay <mateofay2099@gmail.com>
2022-04-11 21:18:49 -0300

    Add UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from '@components/Home/Home';
 import Experience from '@components/Experience/Experience';
 import Portfolio from '@components/Portfolio/Portfolio';
+import Contact from '@components/Contact/Contact';
 import ThemeHandler from '@components/Wrappers/ThemeHandler';
 import { ThemeProvider } from '@providers/Theming.provider.jsx';
 import './App.css';
@@ -13,6 +14,7 @@ const App = () => (
         <Home />
         <Experience />
         <Portfolio />
+        <Contact />
       </main>
     </ThemeHandler>
   </ThemeProvider>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,11 +21,12 @@ const Home = () => {
       </div>
       <div className="homeIntroductionDiv">
         <p>
-          I am {age} years old. I started working on software development when I was 19 years old, although I had
-          already learned the basics of programming in a self-taught way.
+          I am {age} years old. I started working on software development when I was 19 years old,
+          although I had already learned the basics of programming in a self-taught way.
         </p>
         <p>
-          I love what I do and I am constantly looking for learning new technologies and improving my current skills.
+          I love what I do and I am constantly looking for learning new technologies and improving
+          my current skills.
         </p>
       </div>
       <button className="aboutMeButton">
