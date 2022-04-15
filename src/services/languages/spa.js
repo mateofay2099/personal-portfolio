ff4a207b3cdbfb3cd0a4ac19e0f5793829209bd5
Mateo Fay <mateofay2099@gmail.com>
2022-04-15 13:03:06 -0300

    Translate

--- src/services/languages/spa.js
+++ src/services/languages/spa.js
@@ -1,14 +1,14 @@
 export default {
   // Common
-  comingSoon: () => 'Coming Soon',
+  comingSoon: () => 'Próximamente',
 
   // Home
   'home.name.introduction': () => 'Hola, soy',
   'home.profession': () => 'Desarrollador Fullstack',
   'home.introduction.description1': ({ age }) =>
-    `I am ${age} years old. I started working on software development when I was 19 years old, although I had already learned the basics of programming in a self-taught way.`,
+    `Tengo ${age} años. Trabajo en desarrollo de software desde los 19 años, aunque previamente ya había aprendido las bases de programación de forma autodidacta.`,
   'home.introduction.description2': () =>
-    'I love what I do and I am constantly looking for learning new technologies and improving my current skills.',
+    'Me encanta lo que hago, y estoy constantemente buscando aprender nuevas tecnologías para incrementar mis conocimientos y mejorar mis habilidades.',
   'home.aboutMe.button': () => 'Más acerca de mí',
 
   'home.navBar.contact': () => 'Contacto',
@@ -18,13 +18,13 @@ export default {
 
   'home.aboutMeModal.location.title': () => 'Ubicación',
   'home.aboutMeModal.location.description': () =>
-    'I have lived in Campana my whole life, a city in the north of Buenos Aires, Argentina. Thanks to the rise of remote work, I am now able to work from wherever I want, something that I am planning to take advantage of in the near future. Although I am really comfortable with this way of working, I would also be willing to work in an office in another country, because I would really like to settle in a new interesting place.',
+    'He vivido en Campana toda mi vida, una ciudad al norte de la provincia de Buenos Aires, Argentina. Gracias al auje del tele trabajo ahora puedo trabajar desde cualquier lugar, algo de lo que píenso sacar ventaja en un futuro cercano. Aunque estoy muy a gusto trabajando de esta forma, también estaría dispuesto a volver a las oficinas en un nuevo país. Esto se debe a que realmente me gustaría asentarme en un nuevo e interesante lugar.',
   'home.aboutMeModal.hobbies.title': () => 'Hobbies',
   'home.aboutMeModal.hobbies.description': () =>
-    'Besides programming, in my free time I like to hang out with friends, play the guitar, go to the gym and spend some time at home with my two beautiful cats. I also really like to cook, especially meals, but every now and then I enjoy baking a cake',
+    'Adem[as de programar, en mi tiempo libre me gusta juntarme con amigos, tocar la guitarra, ir al gimnasio, y pasar tiempo en casa con mis dos hermosos gatos. Tambien me gusta mucho cocinar, mayormente cosas saladas, pero cada tanto disfruto de hacer algo dulce como una torta',
   'home.aboutMeModal.longTermPlan.title': () => 'Plan a futuro',
   'home.aboutMeModal.longTermPlan.description': () =>
-    'Although I am currently working as a fullstack developer, in the future I would like to move on to data science and machine learning. I find this area really interesting, and extremely important in the future of technology.',
+    'Aunque actualmente estoy trabajando cono desarrollador fullstack, en el futuro me gustaría dedicarme al mundo de data science y machine learning. Encuenrto esta área muy interesante, y extremadamente importante en el futuro de la tecnología.',
 
   // Experience
   'experience.title': () => 'Experiencia',
@@ -35,7 +35,7 @@ export default {
 
   // Portfolio
   'portfolio.title': () => 'Portfolio',
-  'portfolio.card.personalPortfolio.title': () => 'Página de Portfolio Personal',
+  'portfolio.card.personalPortfolio.title': () => 'Personal Portfolio Website',
   'portfolio.card.personalPortfolio.description': () =>
     'Click on the card to see the GitHub repository for this website',
 
